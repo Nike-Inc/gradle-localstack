c8c9785e5f0f9ff7b1feda873ed6b5f94c95de34
gregwhitaker <greg.whitaker@nike.com>
2020-07-17 07:41:18 -0700

    Added func tests

---
+++ gradle-localstack-plugin/src/funcTest/groovy/com/nike/pdm/localstack/aws/dynamodb/DeleteDynamoDbTableFunctionalTest.groovy
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2020-present, Nike, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
+package com.nike.pdm.localstack.aws.dynamodb
+
+import com.nike.pdm.localstack.LocalStackDockerTestUtil
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+class DeleteDynamoDbTableFunctionalTest extends Specification {
+
+    @Rule TemporaryFolder testProjectDir = new TemporaryFolder()
+    File buildFile
+    File composeFile
+
+    LocalStackDockerTestUtil dockerTestUtil = new LocalStackDockerTestUtil()
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+
+        testProjectDir.newFolder('localstack')
+        composeFile = testProjectDir.newFile('localstack/localstack-docker-compose.yml')
+    }
+
+    def cleanup() {
+        dockerTestUtil.killLocalStack()
+    }
+
+    def "should delete dynamodb table"() {
+        given:
+        buildFile << """
+            import com.amazonaws.services.dynamodbv2.model.AttributeDefinition
+            import com.amazonaws.services.dynamodbv2.model.KeySchemaElement
+            import com.amazonaws.services.dynamodbv2.model.KeyType
+            import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType
+            import com.nike.pdm.localstack.aws.dynamodb.CreateDynamoDbTableTask
+
+            plugins {
+                id "java"
+                id "org.springframework.boot"           version "2.2.4.RELEASE"
+                id "io.spring.dependency-management"    version "1.0.9.RELEASE"
+                id "com.avast.gradle.docker-compose"    version "0.12.1"
+            }
+            
+            apply plugin: "com.nike.pdm.localstack"
+            
+            dockerCompose {
+                useComposeFiles = [ 'localstack/localstack-docker-compose.yml' ]
+            }
+            
+            task setupLocalTable(type: CreateDynamoDbTableTask) {
+                tableName = 'catalog.products'
+                keySchema = [
+                        new KeySchemaElement("id", KeyType.HASH)
+                ]
+                attributeDefinitions = [
+                        new AttributeDefinition("id", ScalarAttributeType.S)
+                ]
+            }
+        """
+
+        composeFile << """
+            version: '3.5'
+            
+            services:
+              localstack:
+                image: localstack/localstack:0.11.0
+                container_name: gradle-localstack-plugin-test
+                ports:
+                  - '4566:4566'   # LocalStack Edge
+                  - '8055:8080'   # LocalStack Console
+                networks:
+                  - gradle-localstack-plugin-test
+                environment:
+                  - DEBUG=1
+                  - DATA_DIR=/tmp/localstack/data
+                  - AWS_ACCESS_KEY_ID=dummy
+                  - AWS_SECRET_ACCESS_KEY=dummy
+                volumes:
+                  - './.localstack:/tmp/localstack'
+                  - '/var/run/docker.sock:/var/run/docker.sock'
+                  
+            networks:
+              gradle-localstack-plugin-test:
+                name: gradle-localstack-plugin-test-network
+        """
+
+        when:
+        def setupResult = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('startLocalStack', 'setupLocalTable')
+                .withPluginClasspath()
+                .build()
+
+        def deleteResult = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('deleteDynamoDbTable', '--tableName=catalog.products')
+                .withPluginClasspath()
+                .build()
+
+        then:
+        deleteResult.task(":deleteDynamoDbTable").outcome == SUCCESS
+    }
+}
---
+++ gradle-localstack-plugin/src/funcTest/groovy/com/nike/pdm/localstack/aws/dynamodb/ListDynamoDbTablesFunctionalTest.groovy
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2020-present, Nike, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
+package com.nike.pdm.localstack.aws.dynamodb
+
+import com.nike.pdm.localstack.LocalStackDockerTestUtil
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+class ListDynamoDbTablesFunctionalTest extends Specification {
+
+    @Rule TemporaryFolder testProjectDir = new TemporaryFolder()
+    File buildFile
+    File composeFile
+
+    LocalStackDockerTestUtil dockerTestUtil = new LocalStackDockerTestUtil()
+
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+
+        testProjectDir.newFolder('localstack')
+        composeFile = testProjectDir.newFile('localstack/localstack-docker-compose.yml')
+    }
+
+    def cleanup() {
+        dockerTestUtil.killLocalStack()
+    }
+
+    def "should list dynamodb tables"() {
+        given:
+        buildFile << """
+            import com.amazonaws.services.dynamodbv2.model.AttributeDefinition
+            import com.amazonaws.services.dynamodbv2.model.KeySchemaElement
+            import com.amazonaws.services.dynamodbv2.model.KeyType
+            import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType
+            import com.nike.pdm.localstack.aws.dynamodb.CreateDynamoDbTableTask
+
+            plugins {
+                id "java"
+                id "org.springframework.boot"           version "2.2.4.RELEASE"
+                id "io.spring.dependency-management"    version "1.0.9.RELEASE"
+                id "com.avast.gradle.docker-compose"    version "0.12.1"
+            }
+            
+            apply plugin: "com.nike.pdm.localstack"
+            
+            dockerCompose {
+                useComposeFiles = [ 'localstack/localstack-docker-compose.yml' ]
+            }
+            
+            task setupLocalTable(type: CreateDynamoDbTableTask) {
+                tableName = 'catalog.products'
+                keySchema = [
+                        new KeySchemaElement("id", KeyType.HASH)
+                ]
+                attributeDefinitions = [
+                        new AttributeDefinition("id", ScalarAttributeType.S)
+                ]
+            }
+        """
+
+        composeFile << """
+            version: '3.5'
+            
+            services:
+              localstack:
+                image: localstack/localstack:0.11.0
+                container_name: gradle-localstack-plugin-test
+                ports:
+                  - '4566:4566'   # LocalStack Edge
+                  - '8055:8080'   # LocalStack Console
+                networks:
+                  - gradle-localstack-plugin-test
+                environment:
+                  - DEBUG=1
+                  - DATA_DIR=/tmp/localstack/data
+                  - AWS_ACCESS_KEY_ID=dummy
+                  - AWS_SECRET_ACCESS_KEY=dummy
+                volumes:
+                  - './.localstack:/tmp/localstack'
+                  - '/var/run/docker.sock:/var/run/docker.sock'
+                  
+            networks:
+              gradle-localstack-plugin-test:
+                name: gradle-localstack-plugin-test-network
+        """
+
+        when:
+        def setupResult = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('startLocalStack', 'setupLocalTable')
+                .withPluginClasspath()
+                .build()
+
+        def listResult = GradleRunner.create()
+                .withProjectDir(testProjectDir.root)
+                .withArguments('listDynamoDbTables')
+                .withPluginClasspath()
+                .build()
+
+        then:
+        listResult.task(":listDynamoDbTables").outcome == SUCCESS
+        listResult.output.contains("catalog.products")
+    }
+}
