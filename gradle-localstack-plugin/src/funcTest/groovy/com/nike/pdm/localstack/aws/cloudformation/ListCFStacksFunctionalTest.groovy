599c83c6b3ede142f6ffcde8992962d2c92ce079
gregwhitaker <greg.whitaker@nike.com>
2020-07-17 07:30:37 -0700

    Adding func tests

--- gradle-localstack-plugin/src/funcTest/groovy/com/nike/pdm/localstack/aws/cloudformation/ListCFStacksFunctionalTest.groovy
+++ gradle-localstack-plugin/src/funcTest/groovy/com/nike/pdm/localstack/aws/cloudformation/ListCFStacksFunctionalTest.groovy
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2020-present, Nike, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache-2.0 license found in
+ * the LICENSE file in the root directory of this source tree.
+ */
 package com.nike.pdm.localstack.aws.cloudformation
 
 import com.nike.pdm.localstack.LocalStackDockerTestUtil
