7bd29d2d21b58979c6cc30f776ad24b1de292c61
gregwhitaker <greg.whitaker@nike.com>
2020-07-16 17:28:45 -0700

    Adding tests

---
+++ gradle-localstack-plugin/src/test/java/com/nike/pdm/localstack/boot/SpringBootExtensionTest.java
@@ -0,0 +1,34 @@
+package com.nike.pdm.localstack.boot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SpringBootExtensionTest {
+    
+    @Test
+    public void shouldDefaultEnabled() {
+        SpringBootExtension extension = new SpringBootExtension();
+        assertTrue(extension.isEnabled());
+    }
+
+    @Test
+    public void shouldDefaultDebugPort() {
+        SpringBootExtension extension = new SpringBootExtension();
+        assertEquals(5005, extension.getDebugPort());
+    }
+
+    @Test
+    public void shouldDefaultProfiles() {
+        SpringBootExtension extension = new SpringBootExtension();
+        assertEquals(1, extension.getProfiles().size());
+        assertTrue(extension.getProfiles().contains("local"));
+    }
+
+    @Test
+    public void shouldDefaultJvmArgs() {
+        SpringBootExtension extension = new SpringBootExtension();
+        assertTrue(extension.getJvmArgs().isEmpty());
+    }
+}
