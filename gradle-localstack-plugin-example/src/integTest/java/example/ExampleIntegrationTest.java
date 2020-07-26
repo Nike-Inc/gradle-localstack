715b626c9fcd930144213e537ba33f5fec38bc06
gregwhitaker <gwhitake@gmail.com>
2020-07-26 14:36:02 -0700

    Added example integration test

--- gradle-localstack-plugin-example/src/integTest/java/example/ExampleIntegrationTest.java
+++ gradle-localstack-plugin-example/src/integTest/java/example/ExampleIntegrationTest.java
@@ -7,6 +7,36 @@
  */
 package example;
 
+import example.api.model.GetProductResult;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example integration test that uses LocalStack.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("local")
+@TestPropertySource("classpath:application-local.properties")
 public class ExampleIntegrationTest {
 
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldReturnProduct() {
+        ResponseEntity<GetProductResult> response = testRestTemplate.getForEntity("/products/00001", GetProductResult.class);
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals("00001", response.getBody().getId());
+    }
 }
