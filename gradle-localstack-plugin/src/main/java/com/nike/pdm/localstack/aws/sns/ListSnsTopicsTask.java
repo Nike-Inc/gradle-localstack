fead2aaa8bf4698648afcbe9045995d04f045db2
gregwhitaker <gwhitake@gmail.com>
2020-07-26 19:55:42 -0700

    added list sns topics task

--- gradle-localstack-plugin/src/main/java/com/nike/pdm/localstack/aws/sns/ListSnsTopicsTask.java
+++ gradle-localstack-plugin/src/main/java/com/nike/pdm/localstack/aws/sns/ListSnsTopicsTask.java
@@ -1,11 +1,22 @@
 package com.nike.pdm.localstack.aws.sns;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.ListTopicsResult;
+import com.amazonaws.services.sns.model.Topic;
+import com.nike.pdm.localstack.aws.AwsClientFactory;
 import com.nike.pdm.localstack.compose.LocalStackModule;
+import com.nike.pdm.localstack.core.ConsoleLogger;
+import com.nike.pdm.localstack.core.Retry;
+import de.vandermeer.asciitable.AsciiTable;
+import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 
 import java.util.Arrays;
 
+/**
+ * Task that lists SNS topics.
+ */
 public class ListSnsTopicsTask extends DefaultTask {
 
     public ListSnsTopicsTask() {
@@ -14,7 +25,43 @@ public class ListSnsTopicsTask extends DefaultTask {
 
     @TaskAction
     public void run() {
+        Retry.execute(() -> {
+            final AmazonSNS amazonSNS = AwsClientFactory.getInstance().sns(getProject());
+            final SnsTaskUtil snsTaskUtil = new SnsTaskUtil(getProject());
 
+            AsciiTable at = new AsciiTable();
+            at.setTextAlignment(TextAlignment.JUSTIFIED_LEFT);
+            at.getContext().setWidth(150);
+
+            at.addRule();
+            at.addRow("TopicName", "TopicArn");
+            at.addRule();
+
+            String nextToken = null;
+            int topicCnt = 0;
+            do {
+                final ListTopicsResult listTopicsResult = amazonSNS.listTopics(nextToken);
+
+                for (Topic topic : listTopicsResult.getTopics()) {
+                    String topicName = snsTaskUtil.getTopicNameFromArn(topic.getTopicArn());
+
+                    at.addRow(topicName, topic.getTopicArn());
+                    at.addRule();
+
+                    topicCnt++;
+                }
+
+                nextToken = listTopicsResult.getNextToken();
+            } while (nextToken != null);
+
+            if (topicCnt > 0) {
+                ConsoleLogger.log(at.render());
+            } else {
+                ConsoleLogger.log("No Topics Found!");
+            }
+
+            return null;
+        });
     }
 
     @Override
--- gradle-localstack-plugin/src/main/java/com/nike/pdm/localstack/aws/sns/SnsTaskUtil.java
+++ gradle-localstack-plugin/src/main/java/com/nike/pdm/localstack/aws/sns/SnsTaskUtil.java
@@ -7,6 +7,7 @@
  */
 package com.nike.pdm.localstack.aws.sns;
 
+import com.amazonaws.util.StringUtils;
 import com.nike.pdm.localstack.compose.LocalStackExtension;
 import org.gradle.api.Project;
 
@@ -31,5 +32,19 @@ public class SnsTaskUtil {
         LocalStackExtension ext = project.getExtensions().getByType(LocalStackExtension.class);
 
         return String.format("arn:aws:sns:%s:000000000000:%s", ext.getSigningRegion(), topicName);
+    }
+
+    /**
+     * Gets the topic name from the topic ARN.
+     *
+     * @param topicArn topic arn
+     * @return topic name
+     */
+    public String getTopicNameFromArn(String topicArn) {
+        if (!StringUtils.isNullOrEmpty(topicArn)) {
+            return topicArn.substring(topicArn.lastIndexOf(":") + 1);
+        }
+
+        return null;
     }
 }
