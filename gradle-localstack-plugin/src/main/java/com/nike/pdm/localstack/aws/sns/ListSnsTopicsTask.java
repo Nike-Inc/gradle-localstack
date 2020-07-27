d61a92525963eaf7172e9c5d4a27ace3d7229dcb
gregwhitaker <gwhitake@gmail.com>
2020-07-26 19:20:01 -0700

    Adding list sns topics task

---
+++ gradle-localstack-plugin/src/main/java/com/nike/pdm/localstack/aws/sns/ListSnsTopicsTask.java
@@ -0,0 +1,19 @@
+package com.nike.pdm.localstack.aws.sns;
+
+import com.nike.pdm.localstack.compose.LocalStackModule;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+import java.util.Arrays;
+
+public class ListSnsTopicsTask extends DefaultTask {
+
+    public ListSnsTopicsTask() {
+        setMustRunAfter(Arrays.asList(LocalStackModule.START_LOCALSTACK_TASK_NAME));
+    }
+
+    @TaskAction
+    public void run() {
+
+    }
+}
