b12b758e7de216fd23f0d8287077a3ff556fbadc
gregwhitaker <greg.whitaker@nike.com>
2020-09-05 08:21:34 -0700

    Added timeout to func tests back

--- gradle-localstack-plugin/src/funcTest/groovy/com/nike/pdm/localstack/aws/s3/DeleteS3BucketsFunctionalTest.groovy
+++ gradle-localstack-plugin/src/funcTest/groovy/com/nike/pdm/localstack/aws/s3/DeleteS3BucketsFunctionalTest.groovy
@@ -16,9 +16,13 @@ import org.gradle.testkit.runner.UnexpectedBuildFailure
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
+import spock.lang.Timeout
+
+import java.util.concurrent.TimeUnit
 
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
 
+@Timeout(value= 3, unit = TimeUnit.MINUTES)
 class DeleteS3BucketsFunctionalTest extends Specification {
 
     @Rule TemporaryFolder testProjectDir = new TemporaryFolder()
