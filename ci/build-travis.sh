819ca998d0ed26551b946b8f7613c757cd581725
gregwhitaker <greg.whitaker@nike.com>
2020-09-09 09:47:02 -0700

    Doing work

--- ci/build-travis.sh
+++ ci/build-travis.sh
@@ -6,14 +6,14 @@ cd gradle-localstack-plugin
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew clean build -x funcTest --console plain
+  ./gradlew clean build --console plain
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew clean build -x funcTest --console plain
+  ./gradlew clean build --console plain
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew clean build publishPlugins -x funcTest --console plain
+  ./gradlew clean build publishPlugins -Pgradle.publish.key=$GRADLE_PUBLISH_KEY -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET --console plain
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew clean build -x funcTest --console plain
+  ./gradlew clean build --console plain
 fi
